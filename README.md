b4011810b3e4c1bc34b09c7a297b687b4b2bf0b5
Kate Lim <meekyung.lim@gmail.com>
2013-06-15 22:58:56 -0400

    Initial Code
    IoT Server Discovery through multicast.
    Connect to IoT Server using vert.x

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Controller
 ==========
 
 Controller of Studio Project
+
+1. 이 Project를 Eclipse workspace로 clone.
+2. https://bintray.com/vertx/downloads/distribution/2.0.0-CR1/files 에서 vert.x-2.0.0-CR1.tar.gz 다운로드.
---
+++ src/com/joyl/controller/Controller.java
@@ -0,0 +1,110 @@
+package com.joyl.controller;
+
+import java.net.*; /* import networking package */
+import java.io.*; /* import input/output package */
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.VertxFactory;
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.HttpClientResponse;
+import org.vertx.java.core.http.HttpServerRequest;
+
+public class Controller {
+	public static final String IOTSVR_MCAST_ADDRESS = "224.0.0.30";
+	public static final int IOTSVR_MCAST_PORT = 50000;
+
+	public static final int MAX_LEN = 1024; /* max receive buffer */
+
+	public static void main(String argv[]) {
+
+		InetAddress mcAddress = null; /* multicast address */
+		int mcPort = 0; /* multicast port */
+		InetAddress iotAddress = null; /* IoT Server address */
+		int iotPort = 0; /* IoT Server port */
+		boolean done = false; /* variable for receive loop */
+
+		/* validate the multicast address argument */
+		try {
+			mcAddress = InetAddress.getByName(IOTSVR_MCAST_ADDRESS);
+		} catch (UnknownHostException e) {
+			System.err.println(IOTSVR_MCAST_ADDRESS
+					+ " is not a valid IP address");
+			System.exit(1);
+		}
+
+		/* validate address argument is a multicast IP */
+		if (!mcAddress.isMulticastAddress()) {
+			System.err.println(mcAddress.getHostAddress()
+					+ " is not a multicast IP address.");
+			System.exit(1);
+		}
+
+		/* parse and validate port argument */
+		try {
+			mcPort = IOTSVR_MCAST_PORT;
+		} catch (NumberFormatException nfe) {
+			System.out.println("Invalid port number " + IOTSVR_MCAST_PORT);
+			System.exit(1);
+		}
+
+		try {
+
+			/* instantiate a MulticastSocket */
+			MulticastSocket sock = new MulticastSocket(mcPort);
+
+			/* set the address reuse option */
+			sock.setReuseAddress(true); // Java 1.4 and higher
+
+			/* join the multicast group */
+			sock.joinGroup(mcAddress);
+
+			System.out.println("Start to find IoT Server...");
+			
+			while (!done) { /* loop forever */
+
+				/* create a new DatagramPacket with an empty buffer */
+				byte[] buf = new byte[MAX_LEN];
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+				/* wait to receive packet into the DatagramPacket instance */
+				sock.receive(packet);
+
+				String iotPortStr = new String(packet.getData(), 0, packet.getLength());
+
+				/* get IoT Server Address and Port */
+				iotAddress = packet.getAddress();
+				iotPort = Integer.parseInt(iotPortStr);
+				
+				/* output the data from the packet received */
+				System.out.println("IoT Server Found.. Server Address : " + iotAddress + ": "
+						+ iotPortStr);
+				
+				done = true;
+			}
+
+			sock.leaveGroup(mcAddress);
+			sock.close();
+
+		} catch (IOException e) {
+			System.err.println(e.toString());
+			System.exit(1);
+		}
+
+		// connects to IoT Server
+		System.out.println("Trying to connect server");
+		Vertx vertx = VertxFactory.newVertx("localhost");
+		
+		vertx.createHttpClient().setPort(iotPort).setHost(iotAddress.getHostAddress())
+				.getNow("/", new Handler<HttpClientResponse>() {
+					public void handle(HttpClientResponse response) {
+						response.bodyHandler(new Handler<Buffer>() {
+							public void handle(Buffer data) {
+								System.out.println(data);
+							}
+						});
+					}
+				});
+		
+	}
+}
